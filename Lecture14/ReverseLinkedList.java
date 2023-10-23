4cd38c4bd1938a18341527f3f00ae7a60354493d
alexpapster <alexpapster@gmail.com>
2023-10-22 21:58:27 -0700

    comments for recursive reversal of SLL

--- Lecture14/ReverseLinkedList.java
+++ Lecture14/ReverseLinkedList.java
@@ -20,7 +20,9 @@ public class ReverseLinkedList<Item> extends SinglyLinkedList<Item>{
 			return head;
 	
 		first = reverseLinkedListRecursively(head.next);
+		//reverse link
 		head.next.next = head;
+		//break old pointer
 		head.next = null;
 	
 		return first;
@@ -29,9 +31,9 @@ public class ReverseLinkedList<Item> extends SinglyLinkedList<Item>{
 
     public static void main(String[] args) {
 		ReverseLinkedList<Integer> sll = new ReverseLinkedList<Integer>();
-		sll.add(1);
-		sll.add(2);
 		sll.add(3);
+		sll.add(2);
+		sll.add(1);
         System.out.println(sll);
 		sll.head = sll.reverseLinkedListRecursively(sll.head);
         System.out.println(sll);
